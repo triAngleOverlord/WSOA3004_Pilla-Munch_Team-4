061345008537c3d8081ad03aafbf4510a541c8b2
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-07-24 08:37:03 +0200

    Grid Physics

--- Pilla Muncher/Assets/Scripts/Gridmanager.cs
+++ Pilla Muncher/Assets/Scripts/Gridmanager.cs
@@ -10,6 +10,7 @@ public class Gridmanager : MonoBehaviour
     [SerializeField] private int CellSize;
     [SerializeField] private GameObject blockPrefab;
     [SerializeField] private GameObject gridHolder;
+    [SerializeField] private GameObject prevBlock;
     void Start()
     {
         generateGrid();
@@ -18,7 +19,7 @@ public class Gridmanager : MonoBehaviour
     private void generateGrid()
     {
         var relativeXZero = 0 - CellSize*gridSize.x / 2+ 0.5f*CellSize;
-        var relativeYZero = 0 - CellSize*gridSize.y / 2+ 0.5f*CellSize;
+        var relativeYZero = 0 - 3.5f;
         var x = gridSize.x;
         var y = gridSize.y;
         for (int i = 0; i < y; i++)
@@ -27,6 +28,17 @@ public class Gridmanager : MonoBehaviour
             {
                 var block =  Instantiate(blockPrefab, gridHolder.transform);
                 block.transform.position = new Vector3(relativeXZero + j, relativeYZero + i);
+                var joint = block.GetComponent<FixedJoint2D>();
+                if (j>0)
+                {
+                    joint.connectedBody = prevBlock.GetComponent<Rigidbody2D>();
+                }
+                else
+                {
+                    joint.enabled = false;
+                }
+
+                prevBlock = block;
             }   
         }
     }
